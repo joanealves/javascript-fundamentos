2817f417a65db0a64b8a6bc3a0ea5b6c91c76047
Joane Alves <joanealves2011@gmail.com>
2022-08-23 21:30:49 -0300

    loops e condicionais

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -10,18 +10,33 @@ const idadeComprador = 15;
 console.log("Destinos possíveis");
 console.log(listaDeDestinos);
 
-const estaAcompanhada = true;
-if (idadeComprador >= 18) {
+const estaAcompanhada = false;
+const temPassagemComprada = true;
+// if (idadeComprador >= 18) {
+//     console.log("Comprador maior de idadade");
+//     listaDeDestinos.splice(1, 1); // removendo item
+// } else if (estaAcompanhada == true) {
+//     console.log("Comprador está acompanhado");
+//     listaDeDestinos.splice(1, 1); //removedo item
+// } else {
+//     console.log("Não é maior de idade e não posso vender");
+// }
+
+if (idadeComprador >= 18 || estaAcompanhada == true) {
     console.log("Comprador maior de idade");
-    listaDeDestinos.splice(1, 1)//removendo item dentro da lista
 } else {
-    if (estaAcompanhada) { //a pessoa é menor de idade
-        console.log("Comprador maior de idade");
-        listaDeDestinos.splice(1, 1)//removendo item dentro da lista
-    } else {
-        console.log("Não é maior de idade e não posso vender");
-    }
+    console.log("Não é maior de idade e não posso vender");
 }
+
+console.log("Embarque: \n\n")
+if (idadeComprador >= 18 && temPassagemComprada) {
+    console.log("Boa viagem");
+} else {
+    console.log("Você não pode viajar!");
+};
+
+
+
 
 //operadores logicos
     // > 18
